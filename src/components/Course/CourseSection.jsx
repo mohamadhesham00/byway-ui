45068d1a4b3abd0208b49b525f0ee3bfd34a2ecb
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-09-24 17:37:02 +0300

    Feat: added component for card section row

---
+++ src/components/Course/CourseSection.jsx
@@ -0,0 +1,33 @@
+import { CourseCard } from "./CourseCard";
+export const CourseSection = ({ Title }) => {
+  return (
+    <>
+      <div className="container mt-5">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            margin: "20px auto",
+          }}
+        >
+          <h2>{Title}</h2>
+          <a>See All</a>
+        </div>
+        <div className="row">
+          <div className="col-md-3 col-sm-6">
+            <CourseCard />
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <CourseCard />
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <CourseCard />
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <CourseCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
